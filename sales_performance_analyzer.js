04511b2c1a6f0dcc3e8ba3d08a3458e39e634182
Jazzmyne Roberson <jroberson0927@gmail.com>
2024-09-07 22:57:49 -0400

    Create determinePerformanceRating function

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -3,3 +3,18 @@
 function calculateAverageSales(sales){
     return sales.reduce() / sales.length
 };
+
+
+// Create a function to determine performance rating
+
+function determinePerformanceRating(calculateAverageSales){
+    if (calculateAverageSales >= 10000) {
+        return "Excellent";
+    } else if (calculateAverageSales >= 7000) {
+        return "Good";
+    } else if (calculateAverageSales >= 4000) {
+        return "Satisfactory";
+    } else {
+        return "Needs Improvement";
+    }
+}
